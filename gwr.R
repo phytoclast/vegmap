13a915aad779456dd390e65f666165b50a5077b8
Greg Schmidt <34171749+phytoclast@users.noreply.github.com>
2026-03-02 18:13:40 -0500

    Update gwr.R

--- gwr.R
+++ gwr.R
@@ -2,7 +2,7 @@
 
 #######################
 library(GWmodel)
-ex <- c(-160, -110, 10, 40)
+ex <- c(-180, -150, 10, 40)
 br2 <- crop(br1, y=ext(ex))
 br10 <- br2|> aggregate(10)
 newpts <- br10$elev |> as.points()
@@ -10,13 +10,27 @@ plot(br2$elev)
 #ex <-  c(-130, -60, 10, 50)
 train <- tdata |> subset(x >= ex[1] & x <= ex[2] & y >= ex[3] & y <= ex[4]) #|> st_drop_geometry() |> as.data.frame()
 train$z <- train$t07
+
+train <- trdata. |> subset(x >= ex[1] & x <= ex[2] & y >= ex[3] & y <= ex[4]) #|> st_drop_geometry() |> as.data.frame()
+train$z <- train$tr07
+
+train <- pdata. |> subset(x >= ex[1] & x <= ex[2] & y >= ex[3] & y <= ex[4]) #|> st_drop_geometry() |> as.data.frame()
+train$z <- train$p05
+
 g <- gwr.basic(z ~ lat+coslon+sinlon+
-            relev+
-            elev+
-            w500+w50+w5000+
-            (wind000)+(wind090)+(wind180)+(wind270)+
-            (wind045)+(wind135)+(wind225)+(wind315), data = train, regression.points = geom(newpts)[,c("x", "y")], 
-            adaptive=F, bw=15,kernel = "gaussian", longlat=F)
+                 relev+
+                 elev+
+                 w500+w50+w5000
+                , data = train, regression.points = geom(newpts)[,c("x", "y")], 
+               adaptive=F, bw=5,kernel = "gaussian", longlat=F)
+
+g <- gwr.basic(z ~ lat+coslon+sinlon+
+                 relev+
+                 elev+
+                 w500+w5000+
+                 (wind000)+(wind090)+(wind180)+(wind270)+
+                 (wind045)+(wind135)+(wind225)+(wind315), data = train, regression.points = geom(newpts)[,c("x", "y")], 
+               adaptive=F, bw=15,kernel = "exponential", longlat=F)
 
 # g <- gwr(z ~ lat+coslon+sinlon+
 #            relev+
@@ -35,17 +49,20 @@ g <- gwr.basic(z ~ lat+coslon+sinlon+
 inames = intersect(names(g$SDF),names(br10))
 
 
-br.term <- br10[[inames]] 
-br.term[[inames]] <- g$SDF[inames]
-resid <- br.term[[1]]
-resid[] <- g$SDF$Intercept
+br.term <- rasterize(vect(g$SDF), y=br10, field=inames)
+resid <- rasterize(vect(g$SDF), y=br10, field='Intercept')
 br.term <- br.term |> project(br2)
 resid <- resid |> project(br2)
 br.cov <- br2[[inames]]
 plot(resid)
 z <- sum(br.cov*br.term)+resid
 
-plot(z)
+# plot(z)
+# points(train, cex=0.1)
+
+z1 <- z |> crop(ext(-162, -152, 17, 25))
+plot(z1)
+points(train, cex=0.1)
 
 g <- glm(z ~ lat+coslon+sinlon+
            relev+
